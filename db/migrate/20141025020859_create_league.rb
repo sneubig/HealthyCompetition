2154130a4cd9e260af2e3feea4494a585f3796a3
sneubig <neubig.stephen@gmail.com>
2014-10-25 12:32:02 -0400

    delete migrations

--- db/migrate/20141025020859_create_league.rb
+++
@@ -1,9 +0,0 @@
-class CreateLeague < ActiveRecord::Migration
-  def change
-    create_table :leagues do |t|
-    	t.string :name
-    	t.references :team
-    	t.references :user
-    end
-  end
-end
--- db/migrate/20141025155253_create_record.rb
+++
@@ -1,9 +0,0 @@
-class CreateRecord < ActiveRecord::Migration
-  def change
-    create_table :records do |t|
-    	t.integer :win
-    	t.integer :loss
-    	t.integer :tie
-    end
-  end
-end
