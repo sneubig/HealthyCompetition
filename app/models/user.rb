876c51e9bde45598f0e7e8b11f40748d70020b54
sneubig <neubig.stephen@gmail.com>
2014-10-25 18:27:45 -0400

    add figaro files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
 
+	def init
+		# Instantiate the Choreo, using a previously instantiated TembooSession object, eg:
+		# session = TembooSession.new("sneubig", "myFirstApp", "883113ff303e481cbdf8762158df9793")
+		finalizeOAuthChoreo = Fitbit::OAuth::FinalizeOAuth.new(session)
+
+		# Get an InputSet object for the choreo
+		finalizeOAuthInputs = finalizeOAuthChoreo.new_input_set()
+
+		# Set inputs
+		finalizeOAuthInputs.set_CallbackID(ENV["FITBIT_CALLBACK_ID"]);
+		finalizeOAuthInputs.set_OAuthTokenSecret(ENV["FITBIT_ACCESS_TOKEN_SECRET"]);
+		finalizeOAuthInputs.set_ConsumerSecret(ENV["FITBIT_CONSUMER_SECRET"]);
+		finalizeOAuthInputs.set_ConsumerKey(ENV["FITBIT_CONSUMER_KEY"]);
+
+		# Execute Choreo
+		finalizeOAuthResults = finalizeOAuthChoreo.execute(finalizeOAuthInputs)
+	end
 end
