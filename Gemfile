b43c6ccbc3ad12320e6584c848cd04a4b4134af6
sneubig <neubig.stephen@gmail.com>
2014-10-27 11:11:13 -0400

    took out temboo info

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'omniauth'
 gem 'omniauth-fitbit'
 gem 'fitgem'
 gem 'figaro'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :fitbit, "4f4417a5e2134dc7bbc6d3aa46436979", "e17da7b06107457e8af0407cd4f4934d"
+  provider :fitbit, ENV["FITBIT_CONSUMER_KEY"], ENV["FITBIT_CONSUMER_SECRET"]
 end
