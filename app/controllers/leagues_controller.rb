3ad9ad607dee29a12bce9612d174d467057c0393
sneubig <neubig.stephen@gmail.com>
2014-10-26 20:17:25 -0400

    add league controller and views

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,3 +1,9 @@
 class LeaguesController < ApplicationController
+	def index
+	
+	end
 
+	def show
+		@league = League.find(params[:id])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  resources :teams
   resources :welcome, only: :index
+  resources :leagues do
+    resources :teams, only: [:index, :show, :new, :create]
+  end
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
