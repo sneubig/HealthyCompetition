3ad140c76033d23c9c639e2976b8662fbc2f94b8
sneubig <neubig.stephen@gmail.com>
2014-10-28 19:14:21 -0400

    fitbit api is returning info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'oauth'
 class User < ActiveRecord::Base
 	belongs_to :league
 	has_many :authorizations
@@ -18,9 +19,20 @@ class User < ActiveRecord::Base
 	  	end
     end
 
-    def find_steps
-    	response = HTTParty.get('/1/user/-/activities/steps.json')
+    # def find_steps
+    # 	response = HTTParty.get("http://api.fitbit.com/1/user/#{self.uid}/activities/goals/weekly.json", :headers => {:key => 'ec56b57fd3034d9bbe6c27bb4835546f', :secret => '6c9b616eb9ed72c078d94b81ebd707f2', :token => '31632a2e0761fee574a353dfdf1466ad'})
+    # end
 
-    	puts response
-    end
+    # Exchange your oauth_token and oauth_token_secret for an AccessToken instance.
+	def self.prepare_access_token(oauth_token, oauth_token_secret)
+	    consumer = OAuth::Consumer.new("ec56b57fd3034d9bbe6c27bb4835546f", "91e174c9818d4696bf34d9213c508cc0", { :site => "http://api.fitbit.com", :scheme => :header })
+	     
+	    # now create the access token object from passed values
+	    token_hash = { :oauth_token => oauth_token, :oauth_token_secret => oauth_token_secret }
+	    access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
+	 
+	    return access_token
+	end
+	 
+	
 end
