6f4fdc3c688ebd4eee7405ce1677a9c0605ee20c
sneubig <neubig.stephen@gmail.com>
2014-10-28 20:16:01 -0400

    able to return steps data by itself

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'json'
 gem 'oauth'
 gem 'httparty'
 gem 'pry'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,20 @@
+# Need json gem to parse the json returned from api 
+require 'json'
+
 class WelcomeController < ApplicationController
 
 	def index
 		# raise session.to_yaml
 		# reset_session
 
-		access_token = User.prepare_access_token(session[:token] ,session[:token_secret]  )
+		access_token = User.prepare_access_token(session[:token], session[:token_secret])
 	 
-		# use the access token as an agent to get the home timeline
+		# gets the weekly activity goals 
 		@response = access_token.request(:get, "http://api.fitbit.com/1/user/-/activities/goals/weekly.json") 
-		@activities = access_token.request(:get, "http://api.fitbit.com/1/user/-/activities/date/2014-10-27.json")
 		
-
+		#gets all the activity stats for particular day
+		@activities = access_token.request(:get, "http://api.fitbit.com/1/user/-/activities/date/2014-10-27.json")
+		# Returning steps 
+		@summary = JSON.parse(@activities.body)["summary"]["steps"]
 	end
 end
