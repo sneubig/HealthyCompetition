84045a95e924ad2b085fc26307e57dfa84b13967
sneubig <neubig.stephen@gmail.com>
2014-10-29 23:28:14 -0400

    edit global header

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -11,13 +11,16 @@ class LeaguesController < ApplicationController
 	def create
 		@league = League.new(league_params)
 		if @league.save
-			redirect_to '/league/<%= league.id %>'
+			redirect_to '/league/#{id}'
 		end
 	end
 
 	def new
 		@league = League.new
-		# redirect_to '/league/<%= league_id %>/teams/new'
+		# redirect_to '/league/#{league_id}/teams/new'
+		if @league.save
+			redirect_to '/league/#{id}'
+		end
 	end
 
 private
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,21 +7,20 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     # byebug
     # binding.pry
-	user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth) 
-	session[:user_id] = user.id
-	session[:token] = auth["credentials"]["token"]
+	  user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth) 
+	  session[:user_id] = user.id
+	  session[:token] = auth["credentials"]["token"]
     session[:token_secret] = auth["credentials"]["secret"]
-	redirect_to root_url, :notice => "signed in"
+	  redirect_to root_url, :notice => "signed in"
 
-  	  # session[:name] = auth["info"]["name"]
+  
     # Exchange our oauth_token and oauth_token secret for the AccessToken instance.
-	access_token = User.prepare_access_token(session[:token] ,session[:token_secret]  )
+	  access_token = User.prepare_access_token(session[:token] ,session[:token_secret]  )
 	 
-	# use the access token as an agent to get the home timeline
-	@response = access_token.request(:get, "http://api.fitbit.com/1/user/-/activities/goals/weekly.json")  
+	  # use the access token as an agent to get the home timeline
+	  @response = access_token.request(:get, "http://api.fitbit.com/1/user/-/activities/goals/weekly.json")  
   	@activities = access_token.request(:get, "http://api.fitbit.com/1/user/-/summary/steps.json")
 
-  	 # raise response.to_yaml 
   end
 
   def destroy
